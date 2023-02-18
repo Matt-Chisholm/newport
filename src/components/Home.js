f62fe10fcd6735e313f209fe3298ac2f3d2bef1b
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-02-18 09:21:46 -0800

    fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,6 @@ import * as React from "react";
 import { Container, Typography } from "@mui/material";
 
 export default function HomePage() {
-  console.log('mounted');
   return (
     <Container maxWidth='md'>
       <Typography variant='h4' gutterBottom>
