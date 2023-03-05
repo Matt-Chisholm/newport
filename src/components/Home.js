b6d4f3241ce7c85bcad5415cea5d928ce2b9aad5
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-03-05 10:17:11 -0800

    playing with card

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,34 @@
 import * as React from "react";
-import { Container, Typography } from "@mui/material";
+import { Container, Typography, Card, Box, CardContent } from "@mui/material";
 
 export default function HomePage() {
+  const bull = (
+    <Box component='span' sx={{ display: "inline-block", mx: "2px" }}>
+      &bull;
+    </Box>
+  );
+
+  const card = (
+    <React.Fragment>
+      <CardContent>
+        <Typography variant='h4' gutterBottom>
+          Matt Chisholm {bull} Web and Mobile Developer
+        </Typography>
+        <Typography variant='body1' gutterBottom>
+          Hi, I'm Matt! I'm a developer based in Victoria, BC, and I work with
+          React, React Native, JavaScript, and TypeScript to build web and
+          mobile apps.
+        </Typography>
+      </CardContent>
+    </React.Fragment>
+  );
   return (
     <Container maxWidth='md'>
-      <Typography variant='h4' gutterBottom>
-        Matt Chisholm&nbsp;&bull;&nbsp;Web and Mobile Developer
-      </Typography>
-      <Typography variant='body1' gutterBottom>
-        Hi, I'm Matt! I'm a developer based in Victoria, BC, and I work with
-        React, React Native, JavaScript, and TypeScript to build web and mobile
-        apps.
-      </Typography>
+      <Card
+        sx={{ p: 2, m: 2, backgroundColor: "black", color: "whitesmoke" }}
+        elevation={3}>
+        {card}
+      </Card>
     </Container>
   );
 }
