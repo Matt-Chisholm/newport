73c84e22697241053fc8178cb93623e3c89c3106
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-03-10 09:13:08 -0800

    front and back end skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -19,6 +19,7 @@ export default function Skills() {
     borderTop: "2px solid #0acfcf",
     borderRadius: "0px",
   };
+
   return (
     <Grow in={true} timeout={1200} style={{ transformOrigin: "0 0 0" }}>
       <Container
@@ -53,80 +54,385 @@ export default function Skills() {
         <Box
           sx={{
             display: "flex",
-            alignItems: "flex-start",
-            justifyContent: "flex-start",
+            alignContent: "center",
+            justifyContent: "center",
+            flexDirection: "row",
           }}>
-          <Button
-            color='inherit'
-            className='nav-button'
-            onClick={() => setSelectedSkill("front")}
+          <Box
             sx={{
-              marginRight: "8rem",
-              fontFamily: "Dosis",
-              ...(selectedSkill === "front" && selectedStyle),
+              display: "flex",
+              alignItems: "flex-start",
+              justifyContent: "flex-start",
             }}>
-            <Typography
+            <Button
+              color='inherit'
+              className='nav-button'
+              onClick={() => setSelectedSkill("front")}
               sx={{
+                marginRight: "8rem",
+                marginTop: "1rem",
                 fontFamily: "Dosis",
-                color: "#0ddada",
-                opacity: "90%",
+                ...(selectedSkill === "front" && selectedStyle),
               }}>
-              Front-End
-            </Typography>
-          </Button>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "flex-start",
-            justifyContent: "flex-start",
-          }}>
-          <Button
-            color='inherit'
-            className='nav-button'
-            onClick={() => setSelectedSkill("back")}
+              <Typography
+                sx={{
+                  fontFamily: "Dosis",
+                  color: "#0ddada",
+                  opacity: "90%",
+                }}>
+                Front-End
+              </Typography>
+            </Button>
+          </Box>
+          <Box
             sx={{
-              marginRight: "8rem",
-              marginTop: "1rem",
-              fontFamily: "Dosis",
-              ...(selectedSkill === "back" && selectedStyle),
+              display: "flex",
+              alignItems: "flex-start",
+              justifyContent: "flex-start",
             }}>
-            <Typography
+            <Button
+              color='inherit'
+              className='nav-button'
+              onClick={() => setSelectedSkill("back")}
               sx={{
+                marginRight: "8rem",
+                marginTop: "1rem",
                 fontFamily: "Dosis",
-                color: "#0ddada",
-                opacity: "90%",
+                ...(selectedSkill === "back" && selectedStyle),
               }}>
-              Back-End
-            </Typography>
-          </Button>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "flex-start",
-            justifyContent: "flex-start",
-          }}>
-          <Button
-            color='inherit'
-            className='nav-button'
-            onClick={() => setSelectedSkill("data")}
+              <Typography
+                sx={{
+                  fontFamily: "Dosis",
+                  color: "#0ddada",
+                  opacity: "90%",
+                }}>
+                Back-End
+              </Typography>
+            </Button>
+          </Box>
+          <Box
             sx={{
-              marginRight: "8rem",
-              marginTop: "1rem",
-              fontFamily: "Dosis",
-              ...(selectedSkill === "data" && selectedStyle),
+              display: "flex",
+              alignItems: "flex-start",
+              justifyContent: "flex-start",
             }}>
-            <Typography
+            <Button
+              color='inherit'
+              className='nav-button'
+              onClick={() => setSelectedSkill("data")}
               sx={{
+                marginRight: "8rem",
+                marginTop: "1rem",
                 fontFamily: "Dosis",
-                color: "#0ddada",
-                opacity: "90%",
+                ...(selectedSkill === "data" && selectedStyle),
               }}>
-              Database
-            </Typography>
-          </Button>
+              <Typography
+                sx={{
+                  fontFamily: "Dosis",
+                  color: "#0ddada",
+                  opacity: "90%",
+                }}>
+                Database
+              </Typography>
+            </Button>
+          </Box>
         </Box>
+        {selectedSkill === "front" && (
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "flex-end",
+              marginTop: "2rem",
+              justifyContent: "flex-end",
+            }}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}>
+              <Typography
+                sx={{
+                  fontFamily: "Dosis",
+                  color: "#0ddada",
+                  opacity: "90%",
+                  marginRight: "1rem",
+                }}>
+                HTML
+              </Typography>
+              <Avatar
+                sx={{
+                  width: "3rem",
+                  height: "3rem",
+                  marginRight: "1rem",
+                  marginTop: "1rem",
+                }}
+                src='https://img.icons8.com/color/48/000000/html-5--v1.png'
+              />
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}>
+              <Typography
+                sx={{
+                  fontFamily: "Dosis",
+                  color: "#0ddada",
+                  opacity: "90%",
+                  marginRight: "1rem",
+                }}>
+                CSS
+              </Typography>
+              <Avatar
+                sx={{
+                  width: "3rem",
+                  height: "3rem",
+                  marginRight: "1rem",
+                  marginTop: "1rem",
+                }}
+                src='https://img.icons8.com/color/48/000000/css3.png'
+              />
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}>
+              <Typography
+                sx={{
+                  fontFamily: "Dosis",
+                  color: "#0ddada",
+                  opacity: "90%",
+                  marginRight: "1rem",
+                }}>
+                Javascript
+              </Typography>
+              <Avatar
+                sx={{
+                  width: "3rem",
+                  height: "3rem",
+                  marginRight: "1rem",
+                  marginTop: "1rem",
+                }}
+                src='https://img.icons8.com/color/48/000000/javascript--v1.png'
+              />
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}>
+              <Typography
+                sx={{
+                  fontFamily: "Dosis",
+                  color: "#0ddada",
+                  opacity: "90%",
+                  marginRight: "1rem",
+                }}>
+                Typescript
+              </Typography>
+              <Avatar
+                sx={{
+                  width: "3rem",
+                  height: "3rem",
+                  marginRight: "1rem",
+                  marginTop: "1rem",
+                }}
+                src='https://img.icons8.com/color/48/000000/typescript.png'
+              />
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}>
+              <Typography
+                sx={{
+                  fontFamily: "Dosis",
+                  color: "#0ddada",
+                  opacity: "90%",
+                  marginRight: "1rem",
+                }}>
+                React
+              </Typography>
+              <Avatar
+                sx={{
+                  width: "3rem",
+                  height: "3rem",
+                  marginRight: "1rem",
+                  marginTop: "1rem",
+                }}
+                src='https://img.icons8.com/officel/480/null/react.png'
+              />
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}>
+              <Typography
+                sx={{
+                  fontFamily: "Dosis",
+                  color: "#0ddada",
+                  opacity: "90%",
+                  marginRight: "1rem",
+                }}>
+                Material UI
+              </Typography>
+              <Avatar
+                sx={{
+                  width: "3rem",
+                  height: "3rem",
+                  marginRight: "1rem",
+                  marginTop: "1rem",
+                }}
+                src='https://img.icons8.com/color/48/000000/material-ui.png'
+              />
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}>
+              <Typography
+                sx={{
+                  fontFamily: "Dosis",
+                  color: "#0ddada",
+                  opacity: "90%",
+                  marginRight: "1rem",
+                }}>
+                React Native
+              </Typography>
+              <Avatar
+                sx={{
+                  width: "3rem",
+                  height: "3rem",
+                  marginRight: "1rem",
+                  marginTop: "1rem",
+                }}
+                src='https://img.icons8.com/color/48/000000/react-native.png'
+              />
+            </Box>
+          </Box>
+        )}
+        {selectedSkill === "back" && (
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "flex-end",
+              marginTop: "2rem",
+              justifyContent: "flex-end",
+            }}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}>
+              <Typography
+                sx={{
+                  fontFamily: "Dosis",
+                  color: "#0ddada",
+                  opacity: "90%",
+                  marginRight: "1rem",
+                }}>
+                Node
+              </Typography>
+              <Avatar
+                sx={{
+                  width: "3rem",
+                  height: "3rem",
+                  marginRight: "1rem",
+                  marginTop: "1rem",
+                }}
+                src='https://img.icons8.com/color/48/000000/nodejs.png'
+              />
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}>
+              <Typography
+                sx={{
+                  fontFamily: "Dosis",
+                  color: "#0ddada",
+                  opacity: "90%",
+                  marginRight: "1rem",
+                }}>
+                Express
+              </Typography>
+              <Avatar
+                sx={{
+                  width: "3rem",
+                  height: "3rem",
+                  marginRight: "1rem",
+                  marginTop: "1rem",
+                }}
+                src='https://img.icons8.com/office/480/null/express-js.png'
+              />
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}>
+              <Typography
+                sx={{
+                  fontFamily: "Dosis",
+                  color: "#0ddada",
+                  opacity: "90%",
+                  marginRight: "1rem",
+                }}>
+                Ruby
+              </Typography>
+              <Avatar
+                sx={{
+                  width: "3rem",
+                  height: "3rem",
+                  marginRight: "1rem",
+                  marginTop: "1rem",
+                }}
+                src='https://img.icons8.com/color/48/000000/ruby-programming-language.png'
+              />
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}>
+              <Typography
+                sx={{
+                  fontFamily: "Dosis",
+                  color: "#0ddada",
+                  opacity: "90%",
+                  marginRight: "1rem",
+                  textAlign: "center",
+                }}>
+                Python <br /> (in development)
+              </Typography>
+              <Avatar
+                sx={{
+                  width: "3rem",
+                  height: "3rem",
+                  marginRight: "1rem",
+                  marginTop: "1rem",
+                }}
+                src='https://img.icons8.com/color/48/000000/python--v1.png'
+              />
+            </Box>
+          </Box>
+        )}
       </Container>
     </Grow>
   );
