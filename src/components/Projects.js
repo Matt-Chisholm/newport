a86ba77c9553aeea5c737dfe34fa4d482731546b
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-03-14 09:38:18 -0700

    projects more responsive with slider

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,7 +13,43 @@ import {
 } from "@mui/material";
 import Grow from "@mui/material/Grow";
 
+const projects = [
+  {
+    name: "React Tetris",
+    description:
+      "A working Tetris game built in the browser with React, custom hooks and Styled-Components.",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Typical_Tetris_Game.svg/1200px-Typical_Tetris_Game.svg.png",
+    url: "https://github.com/Matt-Chisholm/react-tetris",
+  },
+  {
+    name: "Tracks",
+    description:
+      "A mobile app for users to track their paths when on hikes built with React Native and MongoDB.",
+    image:
+      "https://globalnews.ca/wp-content/uploads/2016/06/160608_barber-greene_nu.jpg?quality=85&strip=all",
+    url: "https://github.com/Matt-Chisholm/Track-App",
+  },
+  {
+    name: "Paw-Five",
+    description:
+      "A specialized training app for dogs built with React, Express, Bootstrap, Axios and a voice recognition API called Wit.ai.",
+    image:
+      "https://github.com/Matt-Chisholm/Paw-Five/raw/master/docs/readme.md/home.gif?raw=true",
+    url: "https://github.com/Matt-Chisholm/Paw-Five",
+  },
+];
+
 export default function Projects() {
+  const [index, setIndex] = React.useState(0);
+
+  const handleSliderChange = (event, newIndex) => {
+    setIndex(newIndex);
+  };
+
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
     <Grow in={true} timeout={1200} style={{ transformOrigin: "0 0 0" }}>
       <Container
@@ -56,184 +92,130 @@ export default function Projects() {
           }}>
           Click for github repos
         </Typography>
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={6} md={4}>
-            <Card
-              sx={{
-                backgroundColor: "transparent",
-                padding: "1rem",
-                marginBottom: "2rem",
-              }}>
-              <CardContent>
-                <Button
-                  sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                  onClick={() =>
-                    window.open(
-                      "https://github.com/Matt-Chisholm/react-tetris",
-                      "_blank"
-                    )
-                  }>
-                  <Typography
-                    variant='h5'
-                    gutterBottom
-                    sx={{
-                      fontFamily: "TiltWarp",
-                      color: "#0ddada",
-                      opacity: "70%",
-                    }}>
-                    React Tetris
-                  </Typography>
-                  <Avatar
-                    alt='Tetris'
-                    src={
-                      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Typical_Tetris_Game.svg/1200px-Typical_Tetris_Game.svg.png"
-                    }
-                    sx={{
-                      width: 150,
-                      height: 150,
-                      marginBottom: "1rem",
-                    }}
-                  />
-                </Button>
-                <Typography
-                  variant='h5'
-                  gutterBottom
-                  sx={{
-                    fontFamily: "Dosis",
-                    color: "whitesmoke",
-                    opacity: "70%",
-                  }}>
-                  A working Tetris game built in the browser with React, custom
-                  hooks and Styled-Components.
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Card
-              sx={{
-                backgroundColor: "transparent",
-                padding: "1rem",
-                marginBottom: "2rem",
-              }}>
-              <CardContent>
-                <Button
-                  sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                  onClick={() =>
-                    window.open(
-                      "https://github.com/Matt-Chisholm/Track-App",
-                      "_blank"
-                    )
-                  }>
-                  <Typography
-                    variant='h5'
-                    gutterBottom
-                    sx={{
-                      fontFamily: "TiltWarp",
-                      color: "#0ddada",
-                      opacity: "70%",
-                    }}>
-                    Tracks
-                  </Typography>
+        <Container>
+          {isMobile ? (
+            <Slider
+              value={index}
+              onChange={handleSliderChange}
+              aria-label='project slider'
+              sx={{ display: { xs: "block", md: "none" }, color: "#0ddada" }}
+              max={projects.length - 1}
+            />
+          ) : (
+            <div></div>
+          )}
+          {isMobile &&
+            projects.map((project, i) => {
+              if (i === index) {
+                return (
+                  <Grid item key={i} xs={12}>
+                    <Card
+                      sx={{
+                        backgroundColor: "transparent",
+                        padding: "1rem",
+                        marginBottom: "2rem",
+                      }}>
+                      <CardContent>
+                        <Button
+                          sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            justifyContent: "center",
+                            alignItems: "center",
+                          }}
+                          onClick={() => window.open(project.url, "_blank")}>
+                          <Typography
+                            variant='h5'
+                            gutterBottom
+                            sx={{
+                              fontFamily: "TiltWarp",
+                              color: "#0ddada",
+                              opacity: "70%",
+                            }}>
+                            {project.name}
+                          </Typography>
+                          <Avatar
+                            alt={project.name}
+                            src={project.image}
+                            sx={{
+                              width: 150,
+                              height: 150,
+                              marginBottom: "1rem",
+                            }}
+                          />
+                        </Button>
+                        <Typography
+                          variant='h5'
+                          gutterBottom
+                          sx={{
+                            fontFamily: "Dosis",
+                            color: "whitesmoke",
+                            opacity: "70%",
+                          }}>
+                          {project.description}
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                );
+              }
+              return null;
+            })}
 
-                  <Avatar
-                    alt='Tracks'
-                    src={
-                      "https://globalnews.ca/wp-content/uploads/2016/06/160608_barber-greene_nu.jpg?quality=85&strip=all"
-                    }
-                    sx={{
-                      width: 150,
-                      height: 150,
-                      marginBottom: "1rem",
-                    }}
-                  />
-                </Button>
-                <Typography
-                  variant='h6'
-                  gutterBottom
+          <Grid container spacing={3}>
+            {projects.map((project, i) => (
+              <Grid item key={i} xs={12} sm={6} md={4}>
+                <Card
                   sx={{
-                    fontFamily: "Dosis",
-                    color: "whitesmoke",
-                    opacity: "90%",
+                    backgroundColor: "transparent",
+                    padding: "1rem",
+                    marginBottom: "2rem",
                   }}>
-                  A mobile app for users to track their paths when on hikes,
-                  bikerides, roadtrips etc. This app was built with React
-                  Native, Expo, MongoDB and the Google Maps API.
-                </Typography>
-              </CardContent>
-            </Card>
+                  <CardContent>
+                    <Button
+                      sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                      onClick={() => window.open(project.url, "_blank")}>
+                      <Typography
+                        variant='h5'
+                        gutterBottom
+                        sx={{
+                          fontFamily: "TiltWarp",
+                          color: "#0ddada",
+                          opacity: "70%",
+                        }}>
+                        {project.name}
+                      </Typography>
+                      <Avatar
+                        alt={project.name}
+                        src={project.image}
+                        sx={{
+                          width: 150,
+                          height: 150,
+                          marginBottom: "1rem",
+                        }}
+                      />
+                    </Button>
+                    <Typography
+                      variant='h5'
+                      gutterBottom
+                      sx={{
+                        fontFamily: "Dosis",
+                        color: "whitesmoke",
+                        opacity: "70%",
+                      }}>
+                      {project.description}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
           </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Card
-              sx={{
-                backgroundColor: "transparent",
-                width: "50%",
-                padding: "0",
-                marginBottom: "2rem",
-              }}>
-              <CardContent>
-                <Button
-                  sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                  onClick={() =>
-                    window.open(
-                      "https://github.com/Matt-Chisholm/Paw-Five",
-                      "_blank"
-                    )
-                  }>
-                  <Typography
-                    variant='h5'
-                    gutterBottom
-                    sx={{
-                      fontFamily: "TiltWarp",
-                      color: "#0ddada",
-                      opacity: "70%",
-                    }}>
-                    Paw-Five
-                  </Typography>
-
-                  <Avatar
-                    alt='Tetris'
-                    src={
-                      "https://github.com/Matt-Chisholm/Paw-Five/raw/master/docs/readme.md/home.gif?raw=true"
-                    }
-                    sx={{
-                      width: 150,
-                      height: 150,
-                      marginBottom: "1rem",
-                    }}
-                  />
-                </Button>
-                <Typography
-                  variant='h6'
-                  gutterBottom
-                  sx={{
-                    fontFamily: "Dosis",
-                    color: "whitesmoke",
-                    opacity: "90%",
-                  }}>
-                  PAWFIVE is a specialized training app for dogs built with
-                  React, Express, Bootstrap, Axios and a voice recognition API
-                  called Wit.ai.
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        </Grid>
+        </Container>
       </Container>
     </Grow>
   );
